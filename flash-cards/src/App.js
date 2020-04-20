ec89aa83a08298d35c18e39f2b0e4ed13475d43e
Kah919 <kahyap13@gmail.com>
2020-04-19 20:57:51 -0400

    create card design

--- flash-cards/src/App.js
+++ flash-cards/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import FlashcardList from './components/FlashcardList';
+import './app.css';
 
 
 function App() {
@@ -13,17 +14,20 @@ const SAMPLE_FLASHCARDS = [
   {
       id: 1,
       question: "Amazon S3(Simple Storage System",
-      answer: "State and cover info or data from anywhere on the internet"
+      answer: "State and cover info or data from anywhere on the internet",
+      options: [1, 2, 3 , 4]
   },
   {
       id: 2,
       question: "Amazon S3(Simple Storage System",
-      answer: "State and cover info or data from anywhere on the internet"
+      answer: "State and cover info or data from anywhere on the internet",
+      options: [1, 2, 3 , 4]
   },
   {
       id: 3,
       question: "Amazon S3(Simple Storage System",
-      answer: "State and cover info or data from anywhere on the internet"
+      answer: "State and cover info or data from anywhere on the internet",
+      options: [1, 2, 3 , 4]
   }
 ]
 
